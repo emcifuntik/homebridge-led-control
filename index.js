fa74e75f48fe415f4f557cebb841fe2af74137d0
emcifuntik <emcifuntik@gmail.com>
2018-02-27 17:52:56 +0300

    sad

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = (homebridge) => {
   
   // For platform plugin to be considered as dynamic platform plugin,
   // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
-  homebridge.registerPlatform('homebridge-led-control', 'LED-Control', LedControl, true);
+  homebridge.registerPlatform('homebridge-led-control', 'LedControl', LedControl, true);
 }
 
 // Platform constructor
